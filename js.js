1f7c84393b27c87e3d0a74b9b698db6010c87bf3
JuniorJWJ <54831275+JuniorJWJ@users.noreply.github.com>
2019-11-28 16:58:56 -0300

    Alternando cores

--- js.js
+++ js.js
@@ -1,18 +1,18 @@
-function preenche(numero) { 
-	var i=1;
-	var tabela = [
-			["","",""],
-			["","",""],
-			["","",""]
-	];
+var i=2;
+	function preenche(numero) { 
+		var tabela = [
+				["","",""],
+				["","",""],
+				["","",""]
+		];
 
-	if(i%2==0){
-		document.getElementById(numero).style.background="red";
-	}
-	if(i%2==0){
-		document.getElementById(numero).style.color="green";
-	}
-	console.log(i);
-  i=i+1;
+		if(i%2==0){
+			document.getElementById(numero).style.background="red";
+		}
+		if(i%2!=0){
+			document.getElementById(numero).style.background="green";
+		}
+		console.log(i);
+	  i=i+1;
 
-}
+	}
