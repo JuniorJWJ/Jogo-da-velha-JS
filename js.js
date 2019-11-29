660caa13c7c799fcdc3250510bfbd25d7f4763a0
JuniorJWJ <54831275+JuniorJWJ@users.noreply.github.com>
2019-11-28 21:40:52 -0300

    Quase alterando

--- js.js
+++ js.js
@@ -1,18 +1,97 @@
+var empate=0;
 var i=2;
-	function preenche(numero) { 
-		var tabela = [
-				["","",""],
-				["","",""],
-				["","",""]
-		];
+var tabela = [
+    "","","",
+    "","","",
+    "","",""];
 
-		if(i%2==0){
-			document.getElementById(numero).style.background="red";
-		}
-		if(i%2!=0){
-			document.getElementById(numero).style.background="green";
-		}
-		console.log(i);
-	  i=i+1;
+    function preenche(numero) { 
+        
+        if(tabela[numero]=="" && empate==0){
+            if(i%2==0){
+                document.getElementById(numero).src="x.png";
+                tabela[numero]="X";
+            }
+            if(i%2!=0){
+                document.getElementById(numero).style.background="green";
+                tabela[numero]="O";
+            }
+            console.log(i);
+            i=i+1;
+        }
+        console.log(tabela)
+    }
 
+function valida() { 
+    if(tabela[0]=="X" && tabela[1]==tabela[0] && tabela[1]==tabela[02]){//linha1
+        console.log("Jogador 1 venceu!!!")
+        empate = 1
+    }
+    if(tabela[0]=="X" && tabela[0]==tabela[4]  && tabela[4]==tabela[8]){//diagonal1
+        console.log("Jogador 1 venceu!!!")
+        empate = 1
+    }
+    if(tabela[3]=="X" && tabela[3]==tabela[4] && tabela[4]==tabela[5]){//linha2
+        console.log("Jogador 1 venceu!!!")
+        empate = 1
+    }
+    if(tabela[6]=="X" && tabela[6]==tabela[7] && tabela[7]==tabela[8]){//linha3
+        console.log("Jogador 1 venceu!!!")
+        empate = 1
+    }
+    if(tabela[2]=="X" && tabela[2]==tabela[4] && tabela[4]==tabela[6]){//diagonal2
+        console.log("Jogador 1 venceu!!!")
+        empate = 1
+    }
+    if(tabela[0]=="X" && tabela[0]==tabela[3] && tabela[3]==tabela[6]){//coluna1
+        console.log("Jogador 1 venceu!!!")
+        empate = 1
+    }
+    if(tabela[1]=="X" && tabela[1]==tabela[4] && tabela[4]==tabela[7]){//coluna2
+        console.log("Jogador 1 venceu!!!")
+        empate = 1
+    }
+    if(tabela[2]=="X" && tabela[2]==tabela[5] && tabela[5]==tabela[8]){//coluna3
+        console.log("Jogador 1 venceu!!!")
+        empate = 1
+    }
+    //SEGUNDO JOGADOR
+    if(tabela[0]=="O" && tabela[0]==tabela[1] && tabela[1]==tabela[2]){//linha1
+        console.log("O jogador 2 venceu!!!")
+        empate = 1
+    }
+    if(tabela[0]=="O" && tabela[0]==tabela[4] && tabela[4]==tabela[8]){//diagonal1
+        console.log("O jogador 2 venceu!!!")
+        empate = 1
+    }
+    if(tabela[3]=="O" && tabela[3]==tabela[4] && tabela[4]==tabela[5]){//linha2
+        console.log("O jogador 2 venceu!!!")
+        empate = 1
+    }
+    if(tabela[6]=="O" && tabela[6]==tabela[7] && tabela[7]==tabela[8]){//linha3
+        console.log("O jogador 2 venceu!!!")
+        empate = 1
+    }
+    if(tabela[2]=="O" && tabela[2]==tabela[4] && tabela[4]==tabela[6]){//diagonal2
+        console.log("O jogador 2 venceu!!!")
+        empate = 1
+    }
+    if(tabela[0]=="O" && tabela[0]==tabela[3] && tabela[3]==tabela[6]){//coluna1
+        console.log("O jogador 2 venceu!!!")
+        empate = 1
+    }
+    if(tabela[1]=="O" && tabela[1]==tabela[4] && tabela[4]==tabela[7]){//coluna2
+        console.log("O jogador 2 venceu!!!")
+        empate = 1
+    }
+    if(tabela[2]=="O" && tabela[2]==tabela[5] && tabela[5]==tabela[8]){//coluna3
+        console.log("O jogador 2 venceu!!!")
+        empate = 1
+    }
+    if(empate==0 && i==11 ){
+        console.log("Deu Empate")
 	}
+	if(empate==1){
+        console.log("Fim de jogo")
+    }
+}
